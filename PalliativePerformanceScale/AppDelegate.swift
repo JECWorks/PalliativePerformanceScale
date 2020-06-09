2da2eb327ed2884f7360e64e31898c16aaba6249
Jason Cox <jasoncox@mac.com>
2020-06-08 22:58:52 -0400

    Initial Commit

---
+++ PalliativePerformanceScale/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PalliativePerformanceScale
+//
+//  Created by Jason Cox on 6/8/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PalliativePerformanceScale/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PalliativePerformanceScale
+//
+//  Created by Jason Cox on 6/8/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
